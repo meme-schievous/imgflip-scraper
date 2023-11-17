33be070e82cb802764db5c84f6e82b6cb457c465
1gni5 <jules.ducange@posteo.net>
2023-11-18 00:18:52 +0100

    🚸 Add `sort-by` option.

--- imgflip_scraper/commands/feed.py
+++ imgflip_scraper/commands/feed.py
@@ -1,13 +1,23 @@
 from scrapy.commands import ScrapyCommand
 import redis
+import argparse
+from urllib.parse import urlencode
 
 
 class Feed(ScrapyCommand):
     def syntax(self):
-        return "<pages>"
+        return "<pages> [--sort-by=<sort_option>]"
 
     def short_desc(self):
-        return "Feed a specified number of pages to a redis queue."
+        return "Feed a specified number of pages to a redis queue with sorting options."
+
+    def add_options(self, parser):
+        ScrapyCommand.add_options(self, parser)
+        parser.add_argument(
+            "--sort-by",
+            dest="sort_option",
+            help="Sorting option for the meme templates (default: top-30-days)",
+        )
 
     def run(self, args, opts):
         if len(args) != 1:
@@ -21,13 +31,18 @@ class Feed(ScrapyCommand):
             )
 
         # Feed the pages to the redis queue
-        print(f"Feeding {number_of_page} pages to the redis queue...")
+        print("Feeding pages to the redis queue...")
         client = redis.from_url(self.settings.get("REDIS_URL"))
 
         for page in range(1, number_of_page + 1):
-            client.lpush(
-                "imgflip_bootstrap_queue:start_urls",
-                f"https://imgflip.com/memetemplates?sort=top-all-time&page={page}",
-            )
+            params = {"page": page}
+            if opts.sort_option:
+                params["sort"] = opts.sort_option
+            url = f"https://imgflip.com/memetemplates?{urlencode(params)}"
+            client.lpush("imgflip_bootstrap_queue:start_urls", url)
 
         print("Done!")
+
+
+if __name__ == "__main__":
+    Feed().execute()
